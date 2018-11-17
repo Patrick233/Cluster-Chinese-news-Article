78f937fdc2ba332593dfe20d2ac1bf0ad8b33e67
p_yang <patrizio0512@gmail.com>
2018-11-16 21:58:53 -0500

    remove chinese symbols and numbers

--- py_modules/preprocess/preprocess.py
+++ py_modules/preprocess/preprocess.py
@@ -7,14 +7,20 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+ignore_set = {1}
+with open("../../symbols.txt") as ignores:
+    for line in ignores:
+        ignore_set.add(line)
+
 
 def pre_process(file, dir_name):
     file_name = "{}/{}".format(dir_name, file)
     f = open(unicode(file_name, "utf8"), "r")
     doc = ""
     for line in f:
-        # result = re.sub(r'[^\w]', '', line)
-        result = re.sub(r'[0-9]+', '', line)
+        line = line.decode("utf8")
+        result = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"), "".decode("utf8"), line)
+        result = re.sub(r'[0-9]+', '', result)
         result = result.replace(" ", "")
         doc += result
 
@@ -27,10 +33,11 @@ def pre_process(file, dir_name):
 
 
 if __name__ == '__main__':
-    dir_name = "SportNews"
+    # print (ignore_set)
+    dir_name = "/Users/Patrizio/Desktop/neu/cs6220/project/news/test_sport/"
     for file in os.listdir(dir_name):
         file_name = "{}/{}".format(dir_name, file)
-        # print (file_name)
-        if os.path.getsize(file_name) > 100 and os.path.isfile(file_name):
+        print (file_name)
+        if os.path.getsize(file_name) > 100 and os.path.isfile(file_name) and file_name.find(".txt") != -1:
             print (file_name)
             pre_process(file, dir_name)
