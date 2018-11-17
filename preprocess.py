660c719912c8872541bfdef53c847c2acc673cc3
p_yang <patrizio0512@gmail.com>
2018-11-16 20:43:45 -0500

    added preprocess file

--- preprocess.py
+++ preprocess.py
@@ -8,24 +8,29 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-def pre_process(file_name):
-    file = open(unicode(file_name, "utf8"), "r")
+def pre_process(file, dir_name):
+    file_name = "{}/{}".format(dir_name, file)
+    f = open(unicode(file_name, "utf8"), "r")
     doc = ""
-    for line in file:
-        
-        re.sub(r'[^\w]', '', line)
-        line = line.replace(" ", "")
-        doc += line
+    for line in f:
+        # result = re.sub(r'[^\w]', '', line)
+        result = re.sub(r'[0-9]+', '', line)
+        result = result.replace(" ", "")
+        doc += result
 
-    seg_list = jieba.cut(doc, cut_all=False)
-    content = " ".join(seg_list)
-    print("Default Mode: " + content)  # 精确模式
+    dest = dir_name + "/after_process/" + file
+    print (dest)
+    with open(unicode(dest, "utf8"), "w") as filehandle:
+        lines = filter(lambda x: x.strip(), doc)
+        filehandle.writelines(lines)
+        filehandle.close
 
-    try:
-        with open('testBags.txt', 'w') as file_object:
-            # content = content.encode('ascii', 'ignore').decode('ascii')
-            # print (content)
-            file_object.write(content)
-            file_object.close()
-    except Exception, e:
-        print (e)
+
+if __name__ == '__main__':
+    dir_name = "SportNews"
+    for file in os.listdir(dir_name):
+        file_name = "{}/{}".format(dir_name, file)
+        # print (file_name)
+        if os.path.getsize(file_name) > 100 and os.path.isfile(file_name):
+            print (file_name)
+            pre_process(file, dir_name)
